4c42576fdd8e4085af0e135a9084ca6710357a1a
Valar Morghulis <iot.projects.2000@gmail.com>
2024-06-01 18:41:27 +0330

    Add Forward, Revers, stop and setSpeed function

--- sumo.ino
+++ sumo.ino
@@ -1,28 +1,53 @@
-// declaration of IR pins
+// Declaration of IR pins
 #define fIrRP A0
 #define fIrLP A1
 #define bIrRP A2
 #define bIrLP A3
+
+// Declaration of R motor controller pins
+#define eR 11
+#define in2 12
+#define in1 13
+// Declaration of L motor controller pins
+#define eL 10
+#define in4 9
+#define in3 8
+
 unsigned long long int cu = 0;
 
 void setup()
 {
-
     pinMode(fIrRP, INPUT);
     pinMode(fIrLP, INPUT);
     pinMode(bIrRP, INPUT);
     pinMode(bIrLP, INPUT);
 
+    pinMode(eR, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in1, OUTPUT);
+
+    pinMode(eL, OUTPUT);
+    pinMode(in4, OUTPUT);
+    pinMode(in3, OUTPUT);
+
     cu = millis();
     Serial.begin(9600);
+    setSpeed(eL, 255);
+    setSpeed(eR, 255);
+    forward();
+    delay(5000);
+    stop();
 }
 
 void loop()
 {
+    // digitalWrite(in4, HIGH);
+
+    // digitalWrite(in2, HIGH);
 
     if (millis() - cu >= 500)
     {
-        }
+    }
 }
 
 bool irR(int irPin)
@@ -30,4 +55,29 @@ bool irR(int irPin)
     bool irV = 0;
     irV = digitalRead(irPin);
     return irV;
+}
+void setSpeed(char Motor, int speed)
+{
+
+    analogWrite(Motor, speed);
+}
+void forward()
+{
+    digitalWrite(in1, HIGH);
+    digitalWrite(in3, HIGH);
+}
+void rev()
+{
+    digitalWrite(in2, HIGH);
+    digitalWrite(in4, HIGH);
+}
+void stop()
+{
+    digitalWrite(in1, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in4, HIGH);
+    delay(100);
+    digitalWrite(in2, LOW);
+    digitalWrite(in4, LOW);
 }
